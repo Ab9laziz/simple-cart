aff66281502e4fa4ca9431e8c311ffd41337a6a8
Abdulaziz Abdullahi <ab9laziz@gmail.com>
2024-01-18 18:27:11 +0300

    add quantity property for the items

--- src/Data.js
+++ src/Data.js
@@ -11,7 +11,7 @@ import drpepper from './images/drpepper.jpg'
 
 
 
-let Data=[{id:1,name:'Battery',price:'$1.99',img:battery},{id:2,name:'Blanket',price:'$1.99',img:blanket},{id:3,name:'Petrol Can',price:'$1.99',img:can},{id:4,name:'Flashlight',price:'$2.99',img:flashlight},{id:5,name:'Coca Cola',price:'$0.99',img:coke},{id:6,name:'Fanta',price:'$0.99',img:fanta},{id:7,name:'Diet Coke',price:'$0.99',img:dietcoke},{id:8,name:'Pepsi',price:'$0.99',img:pepsi},{id:9,name:'Sprite',price:'$0.99',img:sprite},{id:10,name:'Dr Pepper',price:'$0.99',img:drpepper}]
+let Data=[{id:1,name:'Battery',price:1.99,img:battery,category:'supplies',quantity:0},{id:2,name:'Blanket',price:1.99,img:blanket,category:'supplies',quantity:0},{id:3,name:'Petrol Can',price:1.99,img:can,category:'supplies',quantity:0},{id:4,name:'Flashlight',price:2.99,img:flashlight,category:'supplies',quantity:0},{id:5,name:'Coca Cola',price:0.99,img:coke,category:'drinks',quantity:0},{id:6,name:'Fanta',price:0.99,img:fanta,category:'drinks',quantity:0},{id:7,name:'Diet Coke',price:0.99,img:dietcoke,category:'drinks',quantity:0},{id:8,name:'Pepsi',price:0.99,img:pepsi,category:'drinks',quantity:0},{id:9,name:'Sprite',price:0.99,img:sprite,category:'drinks',quantity:0},{id:10,name:'Dr Pepper',price:0.99,img:drpepper,category:'drinks',quantity:0}]
 
 // console.log(Data)
 export default Data
